a9941c0a703cd8c761940add1c0de9cdea8a13d0
romqatt <romqatt@gmail.com>
2023-12-09 10:28:05 +0300

    Task 2 completed

--- 2_if2.py
+++ 2_if2.py
@@ -15,12 +15,19 @@
 
 """
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
+def main(reference_string, difference_string):
+    if not isinstance(reference_string, str) or not isinstance(difference_string, str):
+      return 0
+    elif reference_string.lower() == difference_string.lower():
+      return 1
+    elif len(reference_string) > len(difference_string) and not difference_string == 'learn':
+       return 2
+    elif difference_string == 'learn':
+       return 3
+
 if __name__ == "__main__":
-    main()
+    print(main('One', 1))               # must be 0
+    print(main('One', 'One'))           # must be 1
+    print(main('Onee', 'One'))          # must be 2
+    print(main('Oneeeeee', 'learn'))    # must be 3
+    print(main('One', 'Four'))          # must be None
