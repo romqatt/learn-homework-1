50d90b2c9ca92aac68444714217fc70c799cc838
romqatt <romqatt@gmail.com>
2023-12-17 13:51:02 +0300

    Bot task ended

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,46 +12,77 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
+import settings
 import logging
+from datetime import datetime
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+import ephem
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filename='bot.log',
+    level=logging.INFO
+)
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(update)
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = f"Hello, {update.effective_user.name}. I'm a bot, please talk to me!"
+    )
 
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(update)
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        reply_to_message_id = update.effective_message.message_id,
+        text = f"I'm help you, {update.effective_user.name}"
+    )
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
+async def planet(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(update)
+    today = datetime.now()
+    ephem_planets = {
+        'Mars': ephem.Mars(today.date()),
+        'Venus': ephem.Venus(today.date()),
+        'Saturn': ephem.Saturn(today.date()),
+        'Jupiter': ephem.Jupiter(today.date()),
+        'Neptune': ephem.Neptune(today.date()),
+        'Uranus': ephem.Uranus(today.date()),
+        'Mercury': ephem.Mercury(today.date()),
+        'Moon': ephem.Moon(today.date())
     }
-}
+    planet = update.message.text.split(' ')[-1]
+    if planet in ephem_planets:        
+        text = ephem.constellation(ephem_planets[planet])
+    else:
+        text = 'Planet does not exis'
+    await context.bot.send_message(
+            chat_id = update.effective_chat.id,
+            reply_to_message_id = update.effective_message.message_id,
+            text = text
+        )
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(update)
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        reply_to_message_id = update.effective_message.message_id,
+        text = update.message.text
+    )
 
-def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(settings.bot_token).build()
+    
+    start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help)
+    planet_handler = CommandHandler('planet', planet)
+    echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
 
-
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
-
-
-def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
-
-    dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-
-    mybot.start_polling()
-    mybot.idle()
-
-
-if __name__ == "__main__":
-    main()
+    application.add_handler(start_handler)
+    application.add_handler(help_handler)
+    application.add_handler(planet_handler)
+    application.add_handler(echo_handler)
+    
+    application.run_polling()
